c39de93e90027247b0ed2119b2543e55e26e7c33
yosecc <yosec.cervino@gmail.com>
2018-10-29 21:40:55 -0400

    Act2910180940

--- resources/views/Backend/layout/layout.blade.php
+++ resources/views/Backend/layout/layout.blade.php
@@ -61,7 +61,7 @@
       <!-- End Navbar -->
       <div class="content">
         <div class="container-fluid">
-          <!-- your content here -->
+          @yield('content')
         </div>
       </div>
       <footer class="footer">
